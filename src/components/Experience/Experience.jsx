14d9c09121e8bb57d39f81c0e75ede5507120208
Aditi Kumari <aditi22202@gmail.com>
2024-12-02 21:07:44 +0530

    Context-API

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,12 +3,22 @@ import {
   FaHtml5,
   FaJs,
   FaReact,
-  FaGoogle,
-  FaAmazon,
   FaGit,
   FaNodeJs,
 } from "react-icons/fa";
-import { SiPython, SiNextdotjs, SiMysql, SiMongodb, SiC, SiRedux, SiTailwindcss, SiTypescript, SiExpress } from "react-icons/si";
+import { MdSchema } from "react-icons/md";
+import { FaPuzzlePiece } from "react-icons/fa";
+import {
+  SiPython,
+  SiNextdotjs,
+  SiMysql,
+  SiMongodb,
+  SiC,
+  SiRedux,
+  SiTailwindcss,
+  SiTypescript,
+  SiExpress,
+} from "react-icons/si";
 import { FaCube } from "react-icons/fa";
 
 const Experience = () => {
@@ -73,45 +83,54 @@ const Experience = () => {
 
         {/* Work Experience Section */}
         <div className="flex flex-col gap-8 w-full md:w-2/5">
-
           {/* Naai India */}
           <div className="flex gap-6 bg-gray-800 bg-opacity-50 shadow-lg rounded-lg p-6 items-center hover:scale-105 transition duration-300">
-            <FaCube color="#E5097k" size={50} />
+            <MdSchema color="#FF9900" size={50} />
             <div className="text-gray-300">
               <h2 className="text-xl font-semibold text-indigo-400">
                 Frontend Developer, Naai India
               </h2>
               <p className="text-sm font-light mb-3">Oct 2024 - Nov 2024</p>
               <ul className="list-disc list-inside">
-                <li>Developed responsive and scalable user interfaces using Next.js, TypeScript, React.js, and Tailwind CSS.</li>
+                <li>
+                  Developed responsive and scalable user interfaces using
+                  Next.js, TypeScript, React.js, and Tailwind CSS.
+                </li>
               </ul>
             </div>
           </div>
 
           {/* Coding Blocks */}
           <div className="flex gap-6 bg-gray-800 bg-opacity-50 shadow-lg rounded-lg p-6 items-center hover:scale-105 transition duration-300">
-            <FaAmazon color="#FF9900" size={50} />
+            <FaCube color="#E5097k" size={50} />
             <div className="text-gray-300">
               <h2 className="text-xl font-semibold text-indigo-400">
                 DSA trainee | Data Structure & Algorithm, Coding Blocks
               </h2>
               <p className="text-sm font-light mb-3">Jun 2024 - Sep 2024</p>
               <ul className="list-disc list-inside">
-                <li>Mastered essential data structures: arrays, linked lists, stacks, queues, trees, graphs, DP, Greedy Algorithms</li>
+                <li>
+                  Mastered essential data structures: arrays, linked lists,
+                  stacks, queues, trees, graphs, DP, Greedy Algorithms
+                </li>
               </ul>
             </div>
           </div>
 
           {/* IITK */}
           <div className="flex gap-6 bg-gray-800 bg-opacity-50 shadow-lg rounded-lg p-6 items-center hover:scale-105 transition duration-300">
-            <FaGoogle color="#4285F4" size={50} />
+            <FaPuzzlePiece color="#4285F4" size={50} />
             <div className="text-gray-300">
               <h2 className="text-xl font-semibold text-indigo-400">
                 Python for Data-Science, IIT Kanpur
               </h2>
               <p className="text-sm font-light mb-3">Jun 2023 - Jul 2023</p>
               <ul className="list-disc list-inside">
-                <li>Gained hands-on experience in various Python libraries such as NumPy, Pandas, and Matplotlib & learned how to apply them to real-world data problems.</li>
+                <li>
+                  Gained hands-on experience in various Python libraries such as
+                  NumPy, Pandas, and Matplotlib & learned how to apply them to
+                  real-world data problems.
+                </li>
               </ul>
             </div>
           </div>
